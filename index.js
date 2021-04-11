26e19022594a06bc4c38b5bc7c3cb8f3ea6db7e9
K Truong <truong.k77@gmail.com>
2021-04-11 00:20:32 -0700

    Add server logic

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const http = require("http");
+const https = require("https");
+const app = require("express")();
+const redirectApp = require("./modules/redirectapp.js");
+const credentials = require("./modules/ssl");
+
+/**
+ * Server Configuration
+ */
+const httpPort = process.env.HTTP_PORT || 80;
+const httpsPort = process.env.HTTPS_PORT || 443;
+
+/**
+ * Server Initialization
+ */
+if (process.env.NODE_ENV == "production") {
+    try {
+        const httpServer = http.createServer(redirectApp);
+        const httpsServer = https.createServer(credentials, app);
+        httpServer.listen(httpPort);
+        httpsServer.listen(httpsPort);
+    } catch (error) {
+        console.log(error.message);
+        dbconnection.end();
+    }
+} else {
+    try {
+        const httpServer = http.createServer(app);
+        httpServer.listen(httpPort);
+    } catch (error) {
+        console.log(error.message);
+        dbconnection.end();
+    }
+}
---
+++ modules/ssl.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+let credentials = null;
+
+/**
+ * SSL Credentials
+ */
+if (process.env.NODE_ENV == "production") {
+    const privateKey = fs.readFileSync(process.env.SSL_PRIV_KEY, "utf-8");
+    const certificate = fs.readFileSync(process.env.SSL_CERT, "utf-8");
+    credentials = {
+        key: privateKey,
+        cert: certificate,
+    };
+}
+
+module.exports = credentials;
